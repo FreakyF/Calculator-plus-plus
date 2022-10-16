61c809dc825c75bf5982ebf7fbbd5badf45eb850
FreakyF <100375045+FreakyF@users.noreply.github.com>
2022-10-16 18:36:49 +0200

    Create Lexer method that converts user input into tokens.

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
+#include <sstream>
 #include "Lexer.h"
-
 
 bool Lexer::validOperator(char character) {
     std::string operators = "*/-+";
@@ -11,4 +11,25 @@ bool Lexer::validOperator(char character) {
         }
     }
     return false;
+}
+
+void Lexer::tokenize() {
+    char nextCharacter = std::cin.peek();
+
+    while (std::cin.good()) {
+        if (isdigit(nextCharacter) || nextCharacter == '.') {
+            // push_back to tokens
+        } else {
+            std::cin.get(nextCharacter);
+            if (std::isspace(nextCharacter)) {
+                while (std::cin.good() && std::isspace(std::cin.peek())) {
+                    std::cin.get();
+                }
+            } else if (validOperator(nextCharacter)) {
+                // push_back to tokens
+            } else {
+                // throw error
+            }
+        }
+    }
 }
